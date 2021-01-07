1ed0209019801f1f672b181e0506c0a84d1fb72c
张飞飞 <zhangfei@xinhua.org>
2021-01-07 14:20:44 +0800

    serivce

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="home">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-button @click="test">apitest</el-button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import api from '@/service/api'
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  methods: {
+    test () {
+      console.log(api.login(), 'login');
+    }
   }
 }
 </script>
